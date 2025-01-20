192df5277ef5d2127459ffe1d387919b6886dee2
khushi2301 <garkhushi12@gmail.com>
2025-01-21 01:53:43 +0530

    Update Footer.jsx

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -8,20 +8,28 @@ const Footer = () => {
         <div className="footer-content">
             <div className="footer-content-left">
                 <img src={assets.logo} alt="" />
-                <p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable</p>
+                <p>Tomato is a vibrant and contemporary restaurant that offers a delightful dining and delivery experience, blending fresh ingredients with innovative culinary techniques. Known for its diverse menu, it serves a range of dishes, from classic favorites to unique creations, all prepared with a focus on quality and taste. Whether you're looking for a casual meal or a special celebration, Tomato provides a welcoming atmosphere, perfect for any occasion.</p>
                 <div className="footer-social-icons">
-                    <img src={assets.facebook_icon} alt="" />
-                    <img src={assets.twitter_icon} alt="" />
-                    <img src={assets.linkedin_icon} alt="" />
+                    <a href="https://m.facebook.com/khushi.gar.9/" target="_blank" rel="noopener noreferrer">
+                    <img src={assets.facebook_icon} alt="Facebook" />
+                    </a>
+                   <a href="https://x.com/GarKhushi53278" target="_blank" rel="noopener noreferrer">
+                      <img src={assets.twitter_icon} alt="Twitter" />
+                    </a>
+                  <a href="https://www.linkedin.com/in/khushi-gar-54904a229/" target="_blank" rel="noopener noreferrer">
+                     <img src={assets.linkedin_icon} alt="LinkedIn" />
+                  </a>
+
                 </div>
             </div>
             <div className="footer-content-center">
                 <h2>COMPANY</h2>
                 <ul>
-                    <li>Home</li>
-                    <li>About us</li>
-                    <li>Delivery</li>
-                    <li>Privacy Policy</li>
+                   <li><a href="/home">Home</a></li>
+                  <li><a href="/#footer">About Us</a></li>
+                  <li><a href="/cart">Delivery</a></li>
+                 <li><a href="/#footer">Privacy Policy</a></li>
+
                 </ul>
             </div>
             <div className="footer-content-right">
