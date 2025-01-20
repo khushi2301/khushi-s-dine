1e305dc12c85e2b691c7261171e6be5f9f2a24a0
khushi2301 <garkhushi12@gmail.com>
2025-01-21 02:51:08 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,19 +5,14 @@ import { Link, useNavigate } from 'react-router-dom';
 import { StoreContext } from '../../context/StoreContext';
 export const Navbar = ({setShowLogin}) => {
     const [menu,setMenu]=useState("menu");
-
     const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
     const navigate=useNavigate();
-
     const logout=()=>{
         localStorage.removeItem("token");
         setToken("");
         navigate("/")
     }
-
   return (
-
-
     <div className='navbar'>
         <Link to='/'><img src={assets.logo} alt='' className='logo'></img></Link>
         <ul className="navbar-menu">
@@ -27,7 +22,6 @@ export const Navbar = ({setShowLogin}) => {
             <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu==="contact-us" ?"active" : ""}>Contact us</a>
         </ul>
         <div className="navbar-right">
-           
             <div className="navbar-search-icon">
                 <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
                 <div className={getTotalCartAmount()===0 ?"":"dot"}></div>
