948d8dc0923129a621a907fd18e6a2253cb73307
khushi2301 <garkhushi12@gmail.com>
2025-01-21 00:00:10 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-app-backend-stjb.onrender.com'
+export const url = 'httpshttps://khushi-s-dine-1.onrender.com'
